f04295bb7352ce8cf976c0a738bc54e5f3036c9c
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-13 16:15:18 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,11 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase = "Squawk!")
-  if phrase = nil 
-    puts "Squawk!"
-    return "Squawk!"
-  else
-    puts "#{phrase}!"
-    return phrase
-  end 
+  puts "#{phrase}!"
+  return phrase
 end 
