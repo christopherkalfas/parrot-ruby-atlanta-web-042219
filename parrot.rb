6dffd086905d636aa0121c458335ff4596bc119b
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-13 16:08:57 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,11 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase = "Squawk")
-  if phrase = "Squawk"
-    puts "Squawk"
-    return "Squawk"
+def parrot(phrase = "Squawk!")
+  if phrase = "Squawk!"
+    puts "Squawk!"
+    return "Squawk!"
   else
-    puts "#{phrase}"
+    puts "#{phrase}!"
     return phrase
   end 
 end 
